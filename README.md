70c529f4a28a36265ecb776fe98dca1daa46aebc
Beloin Sena <beloin.rodrigues@gmail.com>
2023-05-06 23:01:35 -0300

    Using now dht11 driver and showing in Main function

--- README.md
+++ README.md
@@ -3,9 +3,14 @@
 In this project, we show a simple Weather Application, using ESP32 and OLED.
 
 
+// TODO: Something about time interrups: 
+ - https://circuitdigest.com/microcontroller-projects/esp32-timers-and-timer-interrupts
+ - https://docs.espressif.com/projects/esp-idf/en/v4.3/esp32/api-reference/peripherals/timer.html#timer-api-timer-initialization
 
 
 ## References
 
-1. SSD1306 datasheet, can be found [here](https://www.alldatasheet.com/view.jsp?Searchword=Ssd1306%20datasheet&gad=1&gclid=Cj0KCQjw0tKiBhC6ARIsAAOXutlKWRNAzstZ96tXT6xcJW5a0YPrZwLqGcYIT_aOV5m33F_SBqrCLvEaAmuXEALw_wcB)
-1. ESP-IDF Implementation of SSD1306 by `nopnop2002`, can be found [here](https://github.com/nopnop2002/esp-idf-ssd1306)
+1. SSD1306 datasheet, can be found [here](https://www.alldatasheet.com/view.jsp?Searchword=Ssd1306%20datasheet&gad=1&gclid=Cj0KCQjw0tKiBhC6ARIsAAOXutlKWRNAzstZ96tXT6xcJW5a0YPrZwLqGcYIT_aOV5m33F_SBqrCLvEaAmuXEALw_wcB).
+1. ESP-IDF Implementation of SSD1306 by `nopnop2002`, can be found [here](https://github.com/nopnop2002/esp-idf-ssd1306).
+1. DHT11 Datasheet can be found [here](https://www.alldatasheet.com/view.jsp?Searchword=Dht11%20datasheet&gad=1&gclid=Cj0KCQjw9deiBhC1ARIsAHLjR2AGMjLPtpIg5_IqeDmJ1VmYxvTDiGTHjKHhD6Tg9kuFvFfot_n41hsaAssLEALw_wcB).
+1. DHT11 Driver implementation by `UncleRus`, can be found [here](https://github.com/UncleRus/esp-idf-lib/tree/master/components/dht).
--- src/main.c
+++ src/main.c
@@ -8,6 +8,9 @@
 #include <debug.h>
 #include <font8x8_basic.h>
 
+#include <dht.h>
+#include <stdio.h>
+
 // Project:
 
 // Simple Weather Program: Show weather (Temperature and humidity) on OLED.
@@ -20,13 +23,17 @@
 #define SCL_GPIO 22
 #define RESET_GPIO 33
 
+#define SENSOR_GPIO 4
+
 #define CONFIG_SSD1306_128x32 1
 #define CONFIG_SSD1306_128x64 0
 
 void app_main()
 {
-
     debug("Starting Application\n");
+
+    int16_t humidity = 0, temperature = 0;
+    char dh11value[100];
 
     int center, top, bottom;
     char lineChar[20];
@@ -57,104 +64,112 @@ void app_main()
     top = 1;
     center = 1;
     bottom = 4;
-    ssd1306_display_text(&dev, 0, "SSD1306 128x32", 14, false);
-    ssd1306_display_text(&dev, 1, "Hello World!!", 13, false);
-    ssd1306_display_text(&dev, 2, "SSD1306 128x32", 14, true);
-    ssd1306_display_text(&dev, 3, "Hello World!!", 13, true);
+    // ssd1306_display_text(&dev, 0, "SSD1306 128x32", 14, false);
+    // ssd1306_display_text(&dev, 1, "Hello World!!", 13, false);
+    // ssd1306_display_text(&dev, 2, "SSD1306 128x32", 14, true);
+    // ssd1306_display_text(&dev, 3, "Hello World!!", 13, true);
 #endif // CONFIG_SSD1306_128x32
 
     vTaskDelay(3000 / portTICK_PERIOD_MS);
 
-    // Display Count Down
-    uint8_t image[24];
-    memset(image, 0, sizeof(image));
-    ssd1306_display_image(&dev, top, (6 * 8 - 1), image, sizeof(image));
-    ssd1306_display_image(&dev, top + 1, (6 * 8 - 1), image, sizeof(image));
-    ssd1306_display_image(&dev, top + 2, (6 * 8 - 1), image, sizeof(image));
-    for (int font = 0x39; font > 0x30; font--)
+    int tt;
+    while (1)
     {
-        memset(image, 0, sizeof(image));
-        ssd1306_display_image(&dev, top + 1, (7 * 8 - 1), image, 8);
-        memcpy(image, font8x8_basic_tr[font], 8);
-        if (dev._flip)
-            ssd1306_flip(image, 8);
-        ssd1306_display_image(&dev, top + 1, (7 * 8 - 1), image, 8);
+        dht_read_data(DHT_TYPE_DHT11, SENSOR_GPIO, &humidity, &temperature);
+        tt = sprintf(dh11value, "%.1fC | %.1f%c\n", (float)temperature / 10.f, (float)humidity / 10.f, 0x25); // U+0025
+
+        ssd1306_display_text(&dev, 0, dh11value, tt, false);
         vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
 
-    // Scroll Up
-    ssd1306_clear_screen(&dev, false);
-    ssd1306_contrast(&dev, 0xff);
-    ssd1306_display_text(&dev, 0, "---Scroll  UP---", 16, true);
-    // ssd1306_software_scroll(&dev, 7, 1);
-    ssd1306_software_scroll(&dev, (dev._pages - 1), 1);
-    for (int line = 0; line < bottom + 10; line++)
-    {
-        lineChar[0] = 0x01;
-        sprintf(&lineChar[1], " Line %02d", line);
-        ssd1306_scroll_text(&dev, lineChar, strlen(lineChar), false);
-        vTaskDelay(500 / portTICK_PERIOD_MS);
-    }
-    vTaskDelay(3000 / portTICK_PERIOD_MS);
+    // // Display Count Down
+    // uint8_t image[24];
+    // memset(image, 0, sizeof(image));
+    // ssd1306_display_image(&dev, top, (6 * 8 - 1), image, sizeof(image));
+    // ssd1306_display_image(&dev, top + 1, (6 * 8 - 1), image, sizeof(image));
+    // ssd1306_display_image(&dev, top + 2, (6 * 8 - 1), image, sizeof(image));
+    // for (int font = 0x39; font > 0x30; font--)
+    // {
+    //     memset(image, 0, sizeof(image));
+    //     ssd1306_display_image(&dev, top + 1, (7 * 8 - 1), image, 8);
+    //     memcpy(image, font8x8_basic_tr[font], 8);
+    //     if (dev._flip)
+    //         ssd1306_flip(image, 8);
+    //     ssd1306_display_image(&dev, top + 1, (7 * 8 - 1), image, 8);
+    //     vTaskDelay(1000 / portTICK_PERIOD_MS);
+    // }
 
-    // Scroll Down
-    ssd1306_clear_screen(&dev, false);
-    ssd1306_contrast(&dev, 0xff);
-    ssd1306_display_text(&dev, 0, "--Scroll  DOWN--", 16, true);
-    ssd1306_software_scroll(&dev, 1, (dev._pages - 1));
-    for (int line = 0; line < bottom + 10; line++)
-    {
-        lineChar[0] = 0x02;
-        sprintf(&lineChar[1], " Line %02d", line);
-        ssd1306_scroll_text(&dev, lineChar, strlen(lineChar), false);
-        vTaskDelay(500 / portTICK_PERIOD_MS);
-    }
-    vTaskDelay(3000 / portTICK_PERIOD_MS);
+    // // Scroll Up
+    // ssd1306_clear_screen(&dev, false);
+    // ssd1306_contrast(&dev, 0xff);
+    // ssd1306_display_text(&dev, 0, "---Scroll  UP---", 16, true);
+    // // ssd1306_software_scroll(&dev, 7, 1);
+    // ssd1306_software_scroll(&dev, (dev._pages - 1), 1);
+    // for (int line = 0; line < bottom + 10; line++)
+    // {
+    //     lineChar[0] = 0x01;
+    //     sprintf(&lineChar[1], " Line %02d", line);
+    //     ssd1306_scroll_text(&dev, lineChar, strlen(lineChar), false);
+    //     vTaskDelay(500 / portTICK_PERIOD_MS);
+    // }
+    // vTaskDelay(3000 / portTICK_PERIOD_MS);
 
-    // Page Down
-    ssd1306_clear_screen(&dev, false);
-    ssd1306_contrast(&dev, 0xff);
-    ssd1306_display_text(&dev, 0, "---Page	DOWN---", 16, true);
-    ssd1306_software_scroll(&dev, 1, (dev._pages - 1));
-    for (int line = 0; line < bottom + 10; line++)
-    {
-        if ((line % (dev._pages - 1)) == 0)
-            ssd1306_scroll_clear(&dev);
-        lineChar[0] = 0x02;
-        sprintf(&lineChar[1], " Line %02d", line);
-        ssd1306_scroll_text(&dev, lineChar, strlen(lineChar), false);
-        vTaskDelay(500 / portTICK_PERIOD_MS);
-    }
-    vTaskDelay(3000 / portTICK_PERIOD_MS);
+    // // Scroll Down
+    // ssd1306_clear_screen(&dev, false);
+    // ssd1306_contrast(&dev, 0xff);
+    // ssd1306_display_text(&dev, 0, "--Scroll  DOWN--", 16, true);
+    // ssd1306_software_scroll(&dev, 1, (dev._pages - 1));
+    // for (int line = 0; line < bottom + 10; line++)
+    // {
+    //     lineChar[0] = 0x02;
+    //     sprintf(&lineChar[1], " Line %02d", line);
+    //     ssd1306_scroll_text(&dev, lineChar, strlen(lineChar), false);
+    //     vTaskDelay(500 / portTICK_PERIOD_MS);
+    // }
+    // vTaskDelay(3000 / portTICK_PERIOD_MS);
 
-    // Horizontal Scroll
-    ssd1306_clear_screen(&dev, false);
-    ssd1306_contrast(&dev, 0xff);
-    ssd1306_display_text(&dev, center, "Horizontal", 10, false);
-    ssd1306_hardware_scroll(&dev, SCROLL_RIGHT);
-    vTaskDelay(5000 / portTICK_PERIOD_MS);
-    ssd1306_hardware_scroll(&dev, SCROLL_LEFT);
-    vTaskDelay(5000 / portTICK_PERIOD_MS);
-    ssd1306_hardware_scroll(&dev, SCROLL_STOP);
+    // // Page Down
+    // ssd1306_clear_screen(&dev, false);
+    // ssd1306_contrast(&dev, 0xff);
+    // ssd1306_display_text(&dev, 0, "---Page	DOWN---", 16, true);
+    // ssd1306_software_scroll(&dev, 1, (dev._pages - 1));
+    // for (int line = 0; line < bottom + 10; line++)
+    // {
+    //     if ((line % (dev._pages - 1)) == 0)
+    //         ssd1306_scroll_clear(&dev);
+    //     lineChar[0] = 0x02;
+    //     sprintf(&lineChar[1], " Line %02d", line);
+    //     ssd1306_scroll_text(&dev, lineChar, strlen(lineChar), false);
+    //     vTaskDelay(500 / portTICK_PERIOD_MS);
+    // }
+    // vTaskDelay(3000 / portTICK_PERIOD_MS);
 
-    // Vertical Scroll
-    ssd1306_clear_screen(&dev, false);
-    ssd1306_contrast(&dev, 0xff);
-    ssd1306_display_text(&dev, center, "Vertical", 8, false);
-    ssd1306_hardware_scroll(&dev, SCROLL_DOWN);
-    vTaskDelay(5000 / portTICK_PERIOD_MS);
-    ssd1306_hardware_scroll(&dev, SCROLL_UP);
-    vTaskDelay(5000 / portTICK_PERIOD_MS);
-    ssd1306_hardware_scroll(&dev, SCROLL_STOP);
+    // // Horizontal Scroll
+    // ssd1306_clear_screen(&dev, false);
+    // ssd1306_contrast(&dev, 0xff);
+    // ssd1306_display_text(&dev, center, "Horizontal", 10, false);
+    // ssd1306_hardware_scroll(&dev, SCROLL_RIGHT);
+    // vTaskDelay(5000 / portTICK_PERIOD_MS);
+    // ssd1306_hardware_scroll(&dev, SCROLL_LEFT);
+    // vTaskDelay(5000 / portTICK_PERIOD_MS);
+    // ssd1306_hardware_scroll(&dev, SCROLL_STOP);
 
-    // Invert
-    ssd1306_clear_screen(&dev, true);
-    ssd1306_contrast(&dev, 0xff);
-    ssd1306_display_text(&dev, center, "  Good Bye!!", 12, true);
-    vTaskDelay(5000 / portTICK_PERIOD_MS);
+    // // Vertical Scroll
+    // ssd1306_clear_screen(&dev, false);
+    // ssd1306_contrast(&dev, 0xff);
+    // ssd1306_display_text(&dev, center, "Vertical", 8, false);
+    // ssd1306_hardware_scroll(&dev, SCROLL_DOWN);
+    // vTaskDelay(5000 / portTICK_PERIOD_MS);
+    // ssd1306_hardware_scroll(&dev, SCROLL_UP);
+    // vTaskDelay(5000 / portTICK_PERIOD_MS);
+    // ssd1306_hardware_scroll(&dev, SCROLL_STOP);
+
+    // // Invert
+    // ssd1306_clear_screen(&dev, true);
+    // ssd1306_contrast(&dev, 0xff);
+    // ssd1306_display_text(&dev, center, "  Good Bye!!", 12, true);
+    // vTaskDelay(5000 / portTICK_PERIOD_MS);
 
     // Fade Out
     ssd1306_fadeout(&dev);
-
-    esp_restart();
 }
